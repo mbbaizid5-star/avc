7baea30025f1619ae058ea47114968567f38660f
mbbaizid5-star <mbbaizid5@gmail.com>
2026-08-24 07:28:53 +0600

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ CHAT_ID = os.getenv("CHAT_ID")
 MY_USER = os.getenv("MY_USER")
 MY_PASS = os.getenv("MY_PASS")
 
-TARGET_URL = "https://smshadi.net/agent/SMSCDRReports"
-LOGIN_URL = "https://smshadi.net/login"
+TARGET_URL = "httpss://2.59.169.96/ints/agent/SMSCDRReports"
+LOGIN_URL = "https://2.59.169.96/ints/login"
 
 # ✅ Firebase URL
 FB_URL = "https://mhnirob-default-rtdb.firebaseio.com/bot"
